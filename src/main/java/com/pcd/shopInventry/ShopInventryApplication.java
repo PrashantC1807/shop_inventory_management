3a759c111ece9a12ac0899718630fcbd4554ad68
Prash <prashpc7@gmail.com>
2024-10-05 13:46:11 +0530

    Initial commit - Setup project structure

---
+++ src/main/java/com/pcd/shopInventry/ShopInventryApplication.java
@@ -0,0 +1,13 @@
+package com.pcd.shopInventry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopInventryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopInventryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pcd/shopInventry/ShopInventryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcd.shopInventry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopInventryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
